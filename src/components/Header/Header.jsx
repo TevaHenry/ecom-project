2f50a814b589e5678d4e1e0596efdde2aafb97cf
Teva Henry <35941084+TevaHenry@users.noreply.github.com>
2019-08-11 17:12:09 +1200

    Adds CartIcon and CartDropdown Components with cartReducer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
+
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartIcon from '../CartIcon/CartIcon';
+import CartDropdown from '../CartDropdown/CartDropdown';
 import './Header.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to="/">
       <Logo className='logo' />
@@ -18,17 +21,20 @@ const Header = ({ currentUser }) => (
         Contact
       </Link>
       {
-        currentUser ?
+        currentUser ? (
         <div className='option' onClick={() => auth.signOut()}>Sign out</div>
-        : 
+         ) : (
         <Link className='option' to='/signin'>Sign in </Link> 
-      }
+      )}
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 )
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden }}) => ({
+  currentUser,
+  hidden
 });
 
 export default connect(mapStateToProps)(Header);
--- src/redux/User/userActions.js
+++ src/redux/User/userActions.js
@@ -1,4 +1,4 @@
-import { UserActionTypes } from './userTypes';
+import UserActionTypes from './userTypes';
 
 export const setCurrentUser = user => ({
   type: UserActionTypes.SET_CURRENT_USER,
