5052f6628329ab7a47ad23fce169139eee4a9aca
Teva Henry <35941084+TevaHenry@users.noreply.github.com>
2019-08-14 17:35:16 +1200

    Moves Directory and Shop data into own reducers, creates respective selectors, and updates corresponding components

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,9 +1,23 @@
  import { combineReducers } from 'redux';
+ import { persistReducer } from 'redux-persist';
+ import storage from 'redux-persist/lib/storage';
 
  import userReducer from './User/userReducer';
  import cartReducer from './Cart/cartReducer';
+ import directoryReducer from './Directory/directoryReducer';
+ import shopReducer from './Shop/shopReducer';
 
- export default combineReducers({
-   user: userReducer,
-   cart: cartReducer
- });
+ const persistConfig = {
+   key: 'root',
+   storage,
+   whitelist: ['cart']
+ }
+
+ const rootReducer = combineReducers({
+  user: userReducer,
+  cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer
+});
+
+ export default persistReducer(persistConfig, rootReducer);
